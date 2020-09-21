6caec25f3bc4c3a26a9b31e3a402977f97cba0a3
Stephen Gary <sgary0@protonmail.com>
2020-09-21 13:29:08 -0400

    imports for auth-router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,3 +1,9 @@
-const router = require('express').Router();
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const router = require("express").Router();
+
+const Users = require("../users/users-model.js");
+const { isValid } = require("../users/users-service.js");
 
 module.exports = router;
