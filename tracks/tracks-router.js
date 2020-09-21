3e01d1a438755d07e4d2e3c1331cecb68f6ff23e
Stephen Gary <sgary0@protonmail.com>
2020-09-21 12:53:36 -0400

    imports

--- tracks/tracks-router.js
+++ tracks/tracks-router.js
@@ -1,3 +1,5 @@
 const router = require('express').Router();
+const SpotifyWebApi = require('spotify-web-api-node');
+const { CLIENT_ID, CLIENT_SECRET } = require('../vars/vars.js');
 
 module.exports = router;
