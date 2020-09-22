cb77ef5f251ecefe1c138ed0b9f40466b33808c9
Stephen Gary <sgary0@protonmail.com>
2020-09-22 17:44:59 -0400

    database change track_id is no spotify_id

--- mw/validate-track.js
+++ mw/validate-track.js
@@ -4,12 +4,12 @@ module.exports = {
 
 function validateTrack(req, res, next) {
   const body = req.body;
-  const track = body.track_id;
+  const spotify = body.spotify_id;
   const user = body.user_id;
-  
-  if (track === undefined) {
+
+  if (spotify === undefined) {
     res.status(400).json({
-      message: "Whoops! you are missing the track ID :("
+      message: "Whoops! you are missing the spotify ID :("
     });
   }
   if (user === undefined) {
