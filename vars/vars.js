742e32763533339c7a0bad79bd83bd878189ec89
stgary <sgary0@protonmail.com>
2020-11-01 18:50:21 -0500

    rm || statements on process.env

--- vars/vars.js
+++ vars/vars.js
@@ -1,11 +1,10 @@
 module.exports = {
   PORT: process.env.PORT || 5000,
   
-  CLIENT_ID: process.env.CLIENT_ID || '718b14e72ce145d2b969634a8128e73a',
-  CLIENT_SECRET: process.env.CLIENT_SECRET || 'ad2d309521d747489720eba292603981',
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
 
-  BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS || 10,
-  JWT_SECRET: process.env.JWT_SECRET || ':)',
+  BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS,
+  JWT_SECRET: process.env.JWT_SECRET,
 
-  // NODE_ENV: process.env.NODE_ENV || 'development',
 }
